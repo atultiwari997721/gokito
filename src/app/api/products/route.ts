4920c40bd479bcb3158cd652bb023b0a725d95de
atultiwari997721 <atultiwari997721@gmail.com>
2025-12-26 15:53:38 +0530

    Deleted All

--- src/app/api/products/route.ts
+++
@@ -1,7 +0,0 @@
-import { db } from "@/lib/db";
-import { NextResponse } from "next/server";
-
-export async function GET() {
-  const products = db.products.getAll();
-  return NextResponse.json(products);
-}
